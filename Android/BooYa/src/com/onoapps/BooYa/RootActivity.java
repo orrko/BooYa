7ac5ce885ee0ea5eed9727c8249b68ed5355e25c
david svilem <david@onoapp.com>
2011-09-11 14:46:25 +0300

    RootActivity

--- Android/BooYa/src/com/onoapps/BooYa/RootActivity.java
+++ Android/BooYa/src/com/onoapps/BooYa/RootActivity.java
@@ -3,11 +3,14 @@ package com.onoapps.BooYa;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class BooYaLogInActivity extends Activity {
+public class RootActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        
     }
 }
