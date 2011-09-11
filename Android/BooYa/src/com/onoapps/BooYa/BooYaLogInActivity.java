e3162d4430ab8452193e627c937f256e85e23169
david svilem <david@onoapp.com>
2011-09-11 14:38:16 +0300

    delete login act'

--- Android/BooYa/src/com/onoapps/BooYa/BooYaLogInActivity.java
+++ Android/BooYa/src/com/onoapps/BooYa/BooYaLogInActivity.java
@@ -3,7 +3,7 @@ package com.onoapps.BooYa;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class BooYaActivity extends Activity {
+public class BooYaLogInActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
--- Android/BooYa/src/com/onoapps/BooYa/LogInActivity.java
+++
@@ -1,8 +0,0 @@
-package com.onoapps.BooYa;
-
-import android.app.Activity;
-
-
-public class LogInActivity extends Activity {
-
-}
