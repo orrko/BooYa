f8c0b44f5881d214ecf5d040177aad19154ae469
david svilem <david@onoapp.com>
2011-09-11 13:41:14 +0300

    Button Added

--- Android/BooYa/src/com/onoapps/BooYa/BooYaActivity.java
+++ Android/BooYa/src/com/onoapps/BooYa/BooYaActivity.java
@@ -2,6 +2,7 @@ package com.onoapps.BooYa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class BooYaActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,9 @@ public class BooYaActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button myBtn = (Button)findViewById(R.id.button1);
+        
+        
     }
 }
