f2e1d7905f1f9893d26244ac5a2ff29ccde8741e
david svilem <david@onoapp.com>
2011-09-11 15:50:21 +0300

    0

--- Android/BooYa/src/com/onoapps/BooYa/RootActivity.java
+++ Android/BooYa/src/com/onoapps/BooYa/RootActivity.java
@@ -1,52 +1,32 @@
 package com.onoapps.BooYa;
 
-import android.app.Activity;
+import com.onoapps.BooYa.R;
 
+import android.app.Activity;
 import android.content.ContentResolver;
-import android.database.Cursor;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.Contacts;
-import android.provider.Contacts.People;
 
 public class RootActivity extends Activity {
 
+	private SharedPreferences pref;
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        //If log in?
-        
-        ContentResolver cr = getContentResolver();
-        Cursor cur = cr.query(People.CONTENT_URI, 
-			null, null, null, null);
-        if (cur.getCount() > 0) {
-	     while (cur.moveToNext()) {
-	         String id = cur.getString(cur.getColumnIndex(People._ID));
-	         String name = cur.getString(cur.getColumnIndex(People.DISPLAY_NAME));
-	         
-	     	if (Integer.parseInt(cur.getString(
-	                cur.getColumnIndex(People.PRIMARY_PHONE_ID))) > 0) {
-	    		Cursor pCur = cr.query(
-	    				Contacts.Phones.CONTENT_URI, 
-	    				null, 
-	    				Contacts.Phones.PERSON_ID +" = ?", 
-	    				new String[]{id}, null);
-	    		int i=0;
-	    		int pCount = pCur.getCount();
-	    		String[] phoneNum = new String[pCount];
-	    		String[] phoneType = new String[pCount];
-	    		while (pCur.moveToNext()) {
-	    			phoneNum[i] = pCur.getString(
-	                                   pCur.getColumnIndex(Contacts.Phones.NUMBER));
-	    			phoneType[i] = pCur.getString(
-	                                   pCur.getColumnIndex(Contacts.Phones.TYPE));
-	    			i++;
-	    		} 
-	    	}
-	     }
-        }
+        // Check if log in 
+		// Edit that the room has a "Like"
+		SharedPreferences.Editor editor = pref.edit();
+		//editor.putBoolean("RoomId"+myRoomId,true);
+		editor.commit();
+		
+		pref = getSharedPreferences("Likes",0);
+		
+		ContentResolver cr = getContentResolver();
+		
         
     }
 }
