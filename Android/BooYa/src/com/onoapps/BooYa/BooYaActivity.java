8102840a4b76e2410a4de3e3010bca988c671de3
david svilem <david@onoapp.com>
2011-09-11 13:43:16 +0300

    Revert "Button Added"
    This reverts commit f8c0b44f5881d214ecf5d040177aad19154ae469.

--- Android/BooYa/src/com/onoapps/BooYa/BooYaActivity.java
+++ Android/BooYa/src/com/onoapps/BooYa/BooYaActivity.java
@@ -2,7 +2,6 @@ package com.onoapps.BooYa;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class BooYaActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,9 +9,5 @@ public class BooYaActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        Button myBtn = (Button)findViewById(R.id.button1);
-        
-        
     }
 }
