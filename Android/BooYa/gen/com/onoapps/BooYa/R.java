584835b7f34a79ac43b0c36798d53283e157f33d
david svilem <david@onoapp.com>
2011-09-11 20:29:10 +0300

    log in activity

--- Android/BooYa/gen/com/onoapps/BooYa/R.java
+++ Android/BooYa/gen/com/onoapps/BooYa/R.java
@@ -14,10 +14,14 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int phoneNumber=0x7f050003;
+        public static final int registerBtn=0x7f050002;
         public static final int textView1=0x7f050000;
+        public static final int userName=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int register=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- Android/BooYa/src/com/onoapps/BooYa/RootActivity.java
+++ Android/BooYa/src/com/onoapps/BooYa/RootActivity.java
@@ -2,10 +2,17 @@ package com.onoapps.BooYa;
 
 import com.onoapps.BooYa.R;
 
+import android.R.integer;
+import android.R.string;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Contacts;
+import android.provider.Contacts.People;
 
 public class RootActivity extends Activity {
 
@@ -16,17 +23,58 @@ public class RootActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+         
+        // Get the app's shared preferences
+        SharedPreferences app_preferences = 
+        	PreferenceManager.getDefaultSharedPreferences(this);
         
-        // Check if log in 
-		// Edit that the room has a "Like"
-		SharedPreferences.Editor editor = pref.edit();
-		//editor.putBoolean("RoomId"+myRoomId,true);
-		editor.commit();
+        // Get the value for the run counter
+        Boolean isLoggedIn = app_preferences.getBoolean("LOG_IN_FLAG", false);
 		
-		pref = getSharedPreferences("Likes",0);
-		
+        if(!isLoggedIn){      	
+        	//
+        	Intent i = new Intent(this,RegisterActivity.class);
+        	
+        	startActivity(i);
+        }
+        
 		ContentResolver cr = getContentResolver();
-		
+
+		Cursor cur = cr.query(People.CONTENT_URI, 
+				null, null, null, null);
+	        if (cur.getCount() > 0) {
+		     while (cur.moveToNext()) {
+		         String id = cur.getString(cur.getColumnIndex(People._ID));
+		         String name = cur.getString(cur.getColumnIndex(People.DISPLAY_NAME));
+		         System.out.println(id + name);
+		         
+		        String primaryPhoneId = cur.getString(
+		                cur.getColumnIndex(People.PRIMARY_PHONE_ID));
+		        if(primaryPhoneId != null){
+			     	if (Integer.parseInt(cur.getString(
+			                cur.getColumnIndex(People.PRIMARY_PHONE_ID))) > 0) {
+			    		Cursor pCur = cr.query(
+			    				Contacts.Phones.CONTENT_URI, 
+			    				null, 
+			    				Contacts.Phones.PERSON_ID +" = ?", 
+			    				new String[]{id}, null);
+			    		int i=0;
+			    		int pCount = pCur.getCount();
+			    		String[] phoneNum = new String[pCount];
+			    		String[] phoneType = new String[pCount];
+			    		while (pCur.moveToNext()) {
+			    			//TODO: needs to get only cell phone numbers
+			    			phoneNum[i] = pCur.getString(
+			                                   pCur.getColumnIndex(Contacts.Phones.NUMBER));
+			    			phoneType[i] = pCur.getString(
+			                                   pCur.getColumnIndex(Contacts.Phones.TYPE));
+			    			i++;
+			    		} 
+			    	}
+		        }
+
+		     }
+	        }
         
     }
 }
